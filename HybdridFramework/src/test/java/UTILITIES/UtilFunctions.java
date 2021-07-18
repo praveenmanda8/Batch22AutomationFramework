0ae3513700a8fa4dd2c49461eacc34fdaba2a589
E005764 <E005764@LP001223.cignitiglobal.com>
2021-07-18 10:35:43 +0530

    Commiting Utilities Class

--- HybdridFramework/src/test/java/UTILITIES/UtilFunctions.java
+++ HybdridFramework/src/test/java/UTILITIES/UtilFunctions.java
@@ -1,9 +1,31 @@
 package UTILITIES;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class UtilFunctions {
 
-	public static void main(String[] args) {
-		
+	public static void sendKeys(WebDriver driver, WebElement element) {
+         
+	}
+
+	public static void Click(WebDriver driver, WebElement element) {
+
+	}
+
+	public static void mouseHover(WebDriver driver, WebElement element) {
+
+	}
+
+	public static void RightCLick(WebDriver driver, WebElement element) {
+
+	}
+
+	public static void doubleCLick(WebDriver driver, WebElement element) {
+
+	}
+
+	public static void isDisplayed(WebDriver driver, WebElement element) {
 
 	}
 
