2269915d534ad1051eb860f1935431294da61c89
Yadgiri <Yadgiri@DESKTOP-01F49FL>
2021-07-18 09:57:06 +0530

    commiting to 2 new files

---
+++ HybdridFramework/src/test/java/PAGES/Mygit2prac.java
@@ -0,0 +1,10 @@
+package PAGES;
+
+public class Mygit2prac {
+
+	public static void main(String[] args) {
+		System.out.println("hi sasi");
+
+	}
+
+}
---
+++ HybdridFramework/src/test/java/PAGES/mygitclass.java
@@ -0,0 +1,12 @@
+package PAGES;
+
+public class mygitclass {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("hi helo");
+
+	}
+
+}
